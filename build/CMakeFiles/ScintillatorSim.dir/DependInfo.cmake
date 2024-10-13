877ca8771cf3453da6cb9ba2fbbdfb3c5c59583f
María Rodríguez Domínguez <maria@Ordenador-portatil-de-Maria.local>
2024-10-13 14:16:05 +0200

    Created a muon particle gun, a scintillator and a lead target

---
+++ build/CMakeFiles/ScintillatorSim.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/maria/Desktop/Máster/M1/S1/Labo IV/Muon_lifetime/Scintillator.cc" "CMakeFiles/ScintillatorSim.dir/Scintillator.cc.o" "gcc" "CMakeFiles/ScintillatorSim.dir/Scintillator.cc.o.d"
+  "/Users/maria/Desktop/Máster/M1/S1/Labo IV/Muon_lifetime/src/action.cc" "CMakeFiles/ScintillatorSim.dir/src/action.cc.o" "gcc" "CMakeFiles/ScintillatorSim.dir/src/action.cc.o.d"
+  "/Users/maria/Desktop/Máster/M1/S1/Labo IV/Muon_lifetime/src/construction.cc" "CMakeFiles/ScintillatorSim.dir/src/construction.cc.o" "gcc" "CMakeFiles/ScintillatorSim.dir/src/construction.cc.o.d"
+  "/Users/maria/Desktop/Máster/M1/S1/Labo IV/Muon_lifetime/src/detector.cc" "CMakeFiles/ScintillatorSim.dir/src/detector.cc.o" "gcc" "CMakeFiles/ScintillatorSim.dir/src/detector.cc.o.d"
+  "/Users/maria/Desktop/Máster/M1/S1/Labo IV/Muon_lifetime/src/generator.cc" "CMakeFiles/ScintillatorSim.dir/src/generator.cc.o" "gcc" "CMakeFiles/ScintillatorSim.dir/src/generator.cc.o.d"
+  "/Users/maria/Desktop/Máster/M1/S1/Labo IV/Muon_lifetime/src/physics.cc" "CMakeFiles/ScintillatorSim.dir/src/physics.cc.o" "gcc" "CMakeFiles/ScintillatorSim.dir/src/physics.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ScintillatorSim.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ScintillatorSim.dir/Scintillator.cc.o"
+  "CMakeFiles/ScintillatorSim.dir/Scintillator.cc.o.d"
+  "CMakeFiles/ScintillatorSim.dir/src/action.cc.o"
+  "CMakeFiles/ScintillatorSim.dir/src/action.cc.o.d"
+  "CMakeFiles/ScintillatorSim.dir/src/construction.cc.o"
+  "CMakeFiles/ScintillatorSim.dir/src/construction.cc.o.d"
+  "CMakeFiles/ScintillatorSim.dir/src/detector.cc.o"
+  "CMakeFiles/ScintillatorSim.dir/src/detector.cc.o.d"
+  "CMakeFiles/ScintillatorSim.dir/src/generator.cc.o"
+  "CMakeFiles/ScintillatorSim.dir/src/generator.cc.o.d"
+  "CMakeFiles/ScintillatorSim.dir/src/physics.cc.o"
+  "CMakeFiles/ScintillatorSim.dir/src/physics.cc.o.d"
+  "ScintillatorSim"
+  "ScintillatorSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ScintillatorSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
