5e3e53cd69489f46f8db7377a94a54db7d4eac6a
María <maria.rd02@gmail.com>
2024-11-13 11:40:09 +0100

    Added a random initial direction of the muons

--- src/generator.cc
+++ src/generator.cc
@@ -16,8 +16,16 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 	G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
 	G4ParticleDefinition *particle = particleTable->FindParticle("mu-");
 
+	G4double cosTheta = 2*G4UniformRand() - 1., phi = CLHEP::twopi*G4UniformRand();
+  	G4double sinTheta = std::sqrt(1. - cosTheta*cosTheta);
+ 	G4double ux = sinTheta*std::cos(phi),
+             uy = sinTheta*std::sin(phi),
+             uz = cosTheta;
+
+  	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(ux,uy,uz));
+
 	G4ThreeVector pos(0., 0., -5.*m);
-	G4ThreeVector mom(0., 0., 1.);
+	//G4ThreeVector mom(0., 0., 1.);
 
 	G4double E_min = 200. * MeV;
 	G4double E_max = 900. * MeV;
@@ -25,7 +33,7 @@ void MyPrimaryGenerator::GeneratePrimaries(G4Event *anEvent)
 	G4double EnergyMuon = GenerateEnergy(E_min, E_max);
 
 	fParticleGun->SetParticlePosition(pos);
-	fParticleGun->SetParticleMomentumDirection(mom);
+	//fParticleGun->SetParticleMomentumDirection(mom);
 	fParticleGun->SetParticleEnergy(EnergyMuon);
 	fParticleGun->SetParticleDefinition(particle);
 
