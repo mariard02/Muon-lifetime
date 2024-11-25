3f7d2cbba6c7d3523395afb768a45e1718f13144
María <maria.rd02@gmail.com>
2024-11-25 12:07:34 +0100

    Created coincidence analysis

--- analysis/digital_output.py
+++ analysis/digital_output.py
@@ -7,6 +7,22 @@ file = "../build/output/PMT.txt"
 # Open the data
 data = pmt_photons.PMTSplit(file)
 
-data.coincidence(0, 0, 1)
+time_output = np.array([])
 
-plt.show()
+for event in range(data.number_of_events):
+    print(data.delay)
+
+    coincidence_up = data.coincidence(event, 0, 1)
+    coincidence_down = data.coincidence(event, 1, 2)
+
+    start_time = min(coincidence_up)
+
+    for element in coincidence_up:
+        if abs(element - start_time) >= data.delay:
+            time_output = np.append(time_output, element)
+
+    for element in coincidence_down:
+        if abs(element - start_time) >= data.delay:
+            time_output = np.append(time_output, element)
+
+print(time_output)
