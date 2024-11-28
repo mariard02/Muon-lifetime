f4765c0008145ee2f8eae726e323bf72ddf9a873
María <maria.rd02@gmail.com>
2024-11-28 12:29:28 +0100

    Added axes to the plot

--- analysis/time_fit.py
+++ analysis/time_fit.py
@@ -9,6 +9,7 @@ hist, bin = np.histogram(data)
 
 bin = bin[:-1]
 
+# We expect an exponential. Perform the fit using the least squares method (log -> lin).
 a, b = np.polyfit(bin[hist != 0], np.log(hist[hist != 0]), 1)
 
 mean_time = - 1 / a
@@ -19,6 +20,9 @@ total_time = bin[-1]
 time_vec = np.arange(0, total_time, 1e-8)
 
 plt.figure()
-plt.plot(time_vec, np.exp(b) * np.exp(a * time_vec))
-plt.scatter(bin, hist)
+plt.plot(time_vec, np.exp(b) * np.exp(a * time_vec), label = 'Best fit', color = "lightgray")
+plt.scatter(bin, hist, label = "Simulation data")
+plt.xlabel("t (s)")
+plt.ylabel("dN/dt")
+plt.legend()
 plt.show()
