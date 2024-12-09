2644894e16b5a63eb11a94b02636c9a46ba87897
María <maria.rd02@gmail.com>
2024-12-09 14:19:01 +0100

    Changed the way visualization works

--- Scintillator.cc
+++ Scintillator.cc
@@ -19,12 +19,17 @@
 
 int main(int argc, char** argv){
 
-	if (argc != 2){
+	if (argc != 2 && argc != 3){
 		G4cout << " ERROR of the input parameters !!! " << G4endl;
 		return 0;
 	} else {
 		G4cout << "INPUT PARAMETERS " << G4endl;
 		G4cout << "Number of events: " << argv[1] << G4endl;
+		if (argv[2]){
+			G4cout << "With visualization" << G4endl;
+		} else {
+			G4cout << "No visualization" << G4endl;
+		}
 	}
 
 	G4RunManager *runManager = new G4RunManager();
@@ -44,17 +49,43 @@ int main(int argc, char** argv){
   	
   	runManager->Initialize();
 
+	if (argv[2] == 1){
+		G4UIExecutive *ui = new G4UIExecutive(argc, argv);
+
+		G4VisManager *visManager = new G4VisExecutive();
+		visManager->Initialize();
+
+		G4UImanager *UImanager = G4UImanager::GetUIpointer();
+
+    	UImanager->ApplyCommand("/vis/open OGL");
+		UImanager->ApplyCommand("/vis/view/set/viewpointVector 1 1 1");
+		UImanager->ApplyCommand("/vis/drawVolume");
+		UImanager->ApplyCommand("/vis/scene/add/trajectories optical");
+		UImanager->ApplyCommand("/vis/scene/add/trajectories smooth");
+		UImanager->ApplyCommand("/vis/viewer/set/autorefresh true");
+		UImanager->ApplyCommand("/vis/scene/endOfEventAction accumulate");
+		UImanager->ApplyCommand("/tracking/verbose 0");
+		UImanager->ApplyCommand("/event/verbose 0");
+		UImanager->ApplyCommand("/run/verbose 0");
+		UImanager->ApplyCommand("/control/verbose  0");
+
+		runManager -> BeamOn(atoi(argv[1]));
+
+		ui->SessionStart();
+	} else {
+		G4UImanager *UImanager = G4UImanager::GetUIpointer();
+		UImanager->ApplyCommand("/tracking/verbose 0");
+		UImanager->ApplyCommand("/event/verbose 0");
+		UImanager->ApplyCommand("/run/verbose 0");
+		UImanager->ApplyCommand("/control/verbose  0");
+
+		runManager -> BeamOn(atoi(argv[1]));
+	}
+
 	// -------------------------------------------------------------------------------------
 	// NO VISUALIZATION
-	G4UImanager *UImanager = G4UImanager::GetUIpointer();
-	UImanager->ApplyCommand("/tracking/verbose 0");
-	UImanager->ApplyCommand("/event/verbose 0");
-	UImanager->ApplyCommand("/run/verbose 0");
-	UImanager->ApplyCommand("/control/verbose  0");
 
-	runManager -> BeamOn(atoi(argv[1]));
-
-	//UImanager->ApplyCommand("/run/beamOn 1"); // Number of events in one run
+	
 	// -------------------------------------------------------------------------------------
 
 	// -------------------------------------------------------------------------------------
